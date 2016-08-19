c2277c2d381cc3507afbd1aa45de03e78d288201
Jesse Zelaya <jessezelaya1@gmail.com>
2016-08-19 14:28:57 -0700

    Back-End - Created routes to get one user, update and delete

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -30,5 +30,50 @@ router.route('/')
   });
 
 
+// ==================================================
+// ROUTES FOR: /api/users/:user_id
+// ==================================================
+router.route('/:user_id')
+  //GET - single user at /api/users/:user_id
+  .get(function(req,res){
+    User.findById(req.params.user_id, function(err, user){
+      // Send errors if any
+      if(err) res.send(err);
+      // respond with ther user
+      res.json(user);
+    });
+  })
+  //Update - single user at /api/users/:user_id
+  .put(function(req,res){
+    User.findById(req.params.user_id, function(err, user){
+      // Send errors if any
+      if(err) res.send(err);
+
+      if(req.body.name) user.name = req.body.name;
+      if(req.body.username) user.username = req.body.username;
+      if(req.body.email) user.email = req.body.email;
+      if(req.body.password) user.password = req.body.password;
+      if(req.body.highestScore) user.highestScore = req.body.highestScore;
+
+      user.save(function(err){
+        if (err) {
+          res.send(err);
+        }
+        res.json({success: true, message: "Successfully updated account info!"});
+      });
+
+
+    });
+  })
+  //Delete - single user at /api/users/:user_id
+  .delete(function(req,res){
+    User.remove({_id: req.params.user_id}, function(err){
+      // Send any errors
+      if(err) res.send(err);
+      // Send success response
+      res.json({success: true, message: "Account has been Deleted!"});
+    });
+  });
+
 // export instance of Router
 module.exports = router;
