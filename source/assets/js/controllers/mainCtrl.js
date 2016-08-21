a289cde70b5d2bed887edf11ac66cba6641e2e84
Jesse Zelaya <jessezelaya1@gmail.com>
2016-08-20 20:10:47 -0700

    Front-End - Set main controller to check if user is logged in on route changes

--- source/assets/js/controllers/mainCtrl.js
+++ source/assets/js/controllers/mainCtrl.js
@@ -4,9 +4,22 @@
 
   angular
     .module('mainCtrl',[])
-    .controller('mainController', function(){
+    .controller('mainController', function($rootScope, $location, Auth){
       var vm = this;
 
-      console.log('Hello from main controller');
+      // Check if user is logged in on each request
+      // ******************************
+      vm.isLoggedIn = Auth.isUserLoggedIn();
+
+      $rootScope.$on('$routeChangeStart', function(){
+        vm.isLoggedIn = Auth.isUserLoggedIn();
+        Auth.getUserProfileInfo()
+          .then(function(result){
+            // bind user info to view model
+            vm.user = result.data;
+          });
+      });
+
     });
+
 }());
