ae58f3f84362adbf9c8ee4b34d9320a2a64b0730
Jesse Zelaya <jessezelaya1@gmail.com>
2016-08-23 12:56:16 -0700

    Front-End - Added route for sign in page

--- source/assets/js/routes.js
+++ source/assets/js/routes.js
@@ -11,12 +11,19 @@
         .when('/', {
           templateUrl: 'pages/_home.html'
         })
+        // Route for sign up page
+        .when('/signup', {
+          templateUrl: 'pages/_signup.html',
+          controller: 'signUpController',
+          controllerAs: 'signup'
+        })
         // Route for login page
         .when('/login', {
           templateUrl: 'pages/_login.html',
           controller: 'loginController',
           controllerAs: 'login'
         })
+        // Route for main game page
         .when('/game', {
           templateUrl: 'pages/_game.html',
         });
