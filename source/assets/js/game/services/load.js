7e4a3eaa587aa762258396609cf2506c6de480f2
Jesse Zelaya <jessezelaya1@gmail.com>
2016-09-08 17:40:06 -0700

    Front-End - Added theme music to main menu and set text tweens

--- source/assets/js/game/services/load.js
+++ source/assets/js/game/services/load.js
@@ -22,11 +22,13 @@
 
       // load audio assets
       game.load.audio('bgMusic', '../assets/audio/song_starwars_remix.mp3');
+      game.load.audio('mainThemeMusic', '../assets/audio/song_main-theme.mp3');
       game.load.audio('xwingFx', '../assets/audio/sound_xwing-gun.mp3');
       game.load.audio('tieFighterFx', '../assets/audio/sound_tie-fighter-gun.mp3');
       game.load.audio('kaboomFx', '../assets/audio/sound_explosion.mp3');
       game.load.audio('heartFX', '../assets/audio/sound_heart.mp3');
 
+      // Show loading text
       this.showLoadingText();
     };
 
--- source/assets/js/game/services/menu.js
+++ source/assets/js/game/services/menu.js
@@ -6,27 +6,14 @@
   angular.module('gameMenuService',[])
   .factory('menuState', function(){
     var menuFactory = {};
+    var themeMusic;
+    var starfield;
 
     // Main menu
     // ******************************
     menuFactory.create = function(game){
-
-        //Game title
-        var nameLabel = game.add.text(game.world.centerX, 250, 'Star Wars\nFighter Squadron', {
-          font: '50px sf_distant_galaxyregular',
-          fill: '#ffc107'
-        });
-
-        // Instructions to start game
-        var startLabel = game.add.text(game.world.centerX, game.world.height - 80, 'Press "Enter" to start', {
-          font: '20px Arial',
-          fill: '#ddd'
-        });
-
-        // Center game title and instructions
-        nameLabel.anchor.set(0.5);
-        nameLabel.align = 'center';
-        startLabel.anchor.set(0.5);
+        starfield = game.add.tileSprite(0, 0, game.width, game.height, 'starfield');
+        themeMusic = game.add.audio('mainThemeMusic');
 
         // key press to start game
         var enterKey = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
@@ -35,9 +22,57 @@
 
         //start the game
         function start(){
+          // Stop themeMusic
+          themeMusic.pause();
           return game.state.start('play');
         }
 
+        //Play theme music
+        this.playThemeMusic();
+        this.openTitle();
+        this.startGameText();
+    };
+
+    // Play opening music
+    // ******************************
+    menuFactory.playThemeMusic  = function(){
+      themeMusic.play();
+    };
+
+    // Title Crawl
+    // ******************************
+    menuFactory.openTitle = function(){
+      var game = this.game;
+      //Game title
+      var gameTitle = game.add.text(game.world.centerX, 250, 'Star Wars\nFighter Squadron', {
+        font: '50px sf_distant_galaxyregular',
+        fill: '#000',
+        stroke: '#ffc107',
+        strokeThickness: 10
+      });
+      gameTitle.anchor.set(0.5);
+      gameTitle.align = 'center';
+      //Game title crawl in
+      gameTitle.anchor.setTo(0.5, 0.5);
+      gameTitle.scale.setTo(1.5, 1.5);
+      var positionTween = game.add.tween(gameTitle).to({ y: '-40' }, 14000, Phaser.Easing.Linear.Out, true);
+      var gameTitleTween = game.add.tween(gameTitle.scale).to({ x: 0, y: 0 }, 5000, Phaser.Easing.Linear.Out, true);
+      var opacityTween = game.add.tween(gameTitle).to({ alpha: 0 }, 1000, Phaser.Easing.Linear.In, true, 10500);
+    };
+
+    // Flash start game text
+    // ******************************
+    menuFactory.startGameText = function(){
+      var game = this.game;
+      // Instructions to start game
+      var startText = game.add.text(game.world.centerX, game.world.centerY, 'Press ENTER to start', {
+        font: '40px Arial',
+        fill: '#ffc107'
+      });
+      startText.anchor.set(0.5);
+      startText.alpha = 0;
+      var starTween = game.add.tween(startText).to({ alpha: 1 }, 500, Phaser.Easing.Linear.Out, true, 5000, -1 );
+      starTween.yoyo(true, 200);
     };
 
     return menuFactory;
