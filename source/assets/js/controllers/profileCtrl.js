dfb890c635ab90022700281f5bd179c3cd0c71eb
Jesse Zelaya <jessezelaya1@gmail.com>
2016-09-14 00:15:30 -0700

    Front-End - Used stateParams to pass user id to retive info

--- source/assets/js/controllers/profileCtrl.js
+++ source/assets/js/controllers/profileCtrl.js
@@ -4,33 +4,32 @@
 
   angular
     .module('profileCtrl',[])
-    .controller('profileController', function($scope, Auth, User){
+    .controller('profileController', function($scope, $stateParams, Auth, User){
       var vm = this;
       //Loading icon when page opens
       vm.loading = true;
       // Grab the user info to display
       // ******************************
-      Auth.getUserProfileInfo()
-        .then(function(result){
-          User.getOneUser(result.data._id)
-            .success(function(result){
-              // remove loading spinner
-              vm.loading = false;
-              vm.userInfo = result;
-              // Set readable dates here
-              vm.userInfo.createdAt = convertDates(result.createdAt);
-              vm.userInfo.updatedAt = convertDates(result.updatedAt);
-            });
+      User.getOneUser($stateParams.userId)
+        .success(function(result){
+          // remove loading spinner
+          vm.loading = false;
+          vm.userInfo = result;
+          // Set readable dates here
+          vm.userInfo.createdAt = convertDates(result.createdAt);
+          vm.userInfo.updatedAt = convertDates(result.updatedAt);
         });
+
+        // Date conversions
+        // ******************************
+        function convertDates(fecha){
+          var date = new Date(fecha);
+          var fullDate = date.toDateString();
+          return fullDate;
+        }
 
     });
 
-    // Date conversions
-    // ******************************
-    function convertDates(fecha){
-      var date = new Date(fecha);
-      var fullDate = date.toDateString();
-      return fullDate;
-    }
+
 
 }());
